359f24cfcb89376d21d607bf173d8789d0952b23
BaBOWli <1gmoteams@gmail.com>
2024-12-30 19:09:55 +0300

    İlk commit

---
+++ views/emojis.js
@@ -0,0 +1,69 @@
+// @ts-check
+
+const Tik = "✅";
+const Çarpı = "❌";
+const Link = "🔗";
+const Yasak = "🚫";
+const Bakım = "🛠️";
+const Kullanıcı = "👤";
+const İsim = "🧑‍💻";
+const Id = "🆔";
+const Belge = "📄";
+const Premium = "💎";
+const Yazı = "📝";
+const Bot = "🤖";
+const Duyuru = "📢";
+const Davet = "🎉";
+const Çöp = "🗑️";
+const Saat = "⏰";
+const Ping = "🖧";
+const Limit = "⛔";
+const Karaliste = "🚫";
+const Yetkili = "🛡️";
+const Sebep = "🗣️";
+const Tac = "👑";
+const Js = "💻";
+const Node = "🟩";
+const Uptime = "⏱️";
+const Sunucu = "🖥️";
+const Ram = "🧠";
+const Giriş = "🏁";
+const Çıkış = "🚪";
+const Hata = "❗";
+const Destek = "🆘";
+const Oy = "🗳️";
+
+module.exports = {
+  Tik,
+  Çarpı,
+  Link,
+  Yasak,
+  Bakım,
+  Kullanıcı,
+  İsim,
+  Id,
+  Belge,
+  Premium,
+  Yazı,
+  Bot,
+  Duyuru,
+  Davet,
+  Çöp,
+  Saat,
+  Ping,
+  Limit,
+  Karaliste,
+  Yetkili,
+  Sebep,
+  Tac,
+  Js,
+  Node,
+  Uptime,
+  Sunucu,
+  Ram,
+  Giriş,
+  Çıkış,
+  Hata,
+  Destek,
+  Oy,
+};
